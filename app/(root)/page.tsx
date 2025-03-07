ddeceed88ecd2658aa8743757d7bad6d86b64f51
Robert Gleason <l2ob122@gmail.com>
2025-03-06 20:37:08 -0800

    Fetch latest products from the database for the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,16 @@
-import sampleData from '@/db/sample-data';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 import ProductList from '@/components/shared/product/product-list';
 
 export const metadata = {
   title: 'Home',
 };
 
-const Homepage = () => {
-  console.log(sampleData);
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
 
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title='Newest Arrivals'
-        limit={4}
-      />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </>
   );
 };
