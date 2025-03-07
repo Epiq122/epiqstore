29bfead4b70c9b574fd2f4381459c79dc9aabccc
Robert Gleason <l2ob122@gmail.com>
2025-03-06 18:50:47 -0800

    Add product listing feature with sample data and create ProductList component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
+
 export const metadata = {
   title: 'Home',
 };
 
 const Homepage = () => {
-  return <div>epiqStore</div>;
+  console.log(sampleData);
+
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
 };
 export default Homepage;
