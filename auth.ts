52efe4bd65e572d1899f835d0e8d1bf17326440f
Robert Gleason <l2ob122@gmail.com>
2025-03-09 18:44:20 -0700

    Add user role and name handling in session and JWT for improved user management

--- auth.ts
+++ auth.ts
@@ -55,12 +55,32 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
       // if there is an update set the user name
       if (trigger === 'update') {
         session.user.name = user.name;
       }
 
       return session;
+    },
+    async jwt({ token, user }: any) {
+      // assign user fields to the token
+      if (user) {
+        token.role = user.role;
+
+        // if user has no name then  use email
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+
+          // update the db with the new name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
